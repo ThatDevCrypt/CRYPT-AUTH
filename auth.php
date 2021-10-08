6e4ba6d55d7224c4fd582b3c953ea38ae8107686
CRYPT <92074562+ThatDevCrypt@users.noreply.github.com>
2021-10-08 21:20:55 +0530

    Update auth.php

--- auth.php
+++ auth.php
@@ -86,7 +86,7 @@ if (isset($_POST['login'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <title>Dev Site | Login / Register</title>
+    <title>CRYPT SAMPLE AUTH | Login / Register</title>
 </head>
 <body>
     <style>
